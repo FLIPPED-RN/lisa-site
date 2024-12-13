56ddf7fd6ab16eded369c2cece60fc5f99cc4632
Nazarov Ruslan <rnazarov142@gmail.com>
2024-12-14 00:44:38 +0200

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import styles from "../styles/header.module.css";
+
+export default function Header() {
+  return(
+    <div className={styles.header}>
+      <div>
+        <Image 
+          src={'/lisa-logo.svg'}
+          width={200}
+          height={100}
+          alt=""
+        />
+      </div>
+      <div className={styles.links}>
+        <a href="">о нас</a>
+        <a href="">оставить заявку</a>
+        <a href="">контакты</a>
+      </div>
+    </div>
+  )
+}
